e16b80dce4a1aa80ef31d27d4dfda1fef8e51e77
Zaki1726 <100398414+Zaki1726@users.noreply.github.com>
2022-08-25 07:47:55 +0700

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -1943,35 +1943,35 @@ break
             break
             //baru?
             case 'dosa1': 
-qute = fs.readFileSync('./gambar/fake.jpg') 
+qute = { url: 'https://telegra.ph/file/cb7371aa27942b81a3f61.jpg' }
 hisoka.sendMessage(from, qute, image, { quoted: mek, caption: '*SEMOGA DI BERI HIDAYAH*\nLink Download \n\nhttps://www.mediafire.com/file/h2nygxbyb6n9cyo/VID-20210107-WA1468.mp4/file' })
 break
 case 'dosa2':
-qute = fs.readFileSync('./gambar/fake.jpg') 
+qute = { url: 'https://telegra.ph/file/cb7371aa27942b81a3f61.jpg' }
 hisoka.sendMessage(from, qute, image, { quoted: mek, caption: '*SEMOGA DI BERI HIDAYAH*\nLink Download \n\nhttps://www.mediafire.com/file/pk8hozohzdc076c/VID-20210107-WA1466.mp4/file' })
 break
 case 'dosa3':
-qute = fs.readFileSync('./gambar/fake.jpg') 
+qute = { url: 'https://telegra.ph/file/cb7371aa27942b81a3f61.jpg' }
 hisoka.sendMessage(from, qute, image, { quoted: mek, caption: '*SEMOGA DI BERI HIDAYAH*\nLink Download \n\nhttps://www.mediafire.com/file/112q3u286tnvzjo/VID-20210107-WA1467.3gp/file' }) 
 break
 case 'dosa4':	
-qute = fs.readFileSync('./gambar/fake.jpg') 
+qute = { url: 'https://telegra.ph/file/cb7371aa27942b81a3f61.jpg' }
 hisoka.sendMessage(from, qute, image, { quoted: mek, caption: '*SEMOGA DI BERI HIDAYAH*\nLink Download \n\nhttps://www.mediafire.com/file/arpphhxsv94ak0r/VID-20210107-WA1462.mp4/file' })
 break
 case 'dosa5':	
-qute = fs.readFileSync('./gambar/fake.jpg') 
+qute = { url: 'https://telegra.ph/file/cb7371aa27942b81a3f61.jpg' }
 hisoka.sendMessage(from, qute, image, { quoted: mek, caption: '*SEMOGA DI BERI HIDAYAH*\nLink Download \n\nhttps://www.mediafire.com/file/us3f4j62emftbrf/VID-20210107-WA1463.mp4/file' })
 break
 case 'dosa6':	
-qute = fs.readFileSync('./gambar/fake.jpg') 
+qute = { url: 'https://telegra.ph/file/cb7371aa27942b81a3f61.jpg' }
 hisoka.sendMessage(from, qute, image, { quoted: mek, caption: '*SEMOGA DI BERI HIDAYAH*\nLink Download \n\nhttps://www.mediafire.com/file/v4033tkl16hgf2b/VID-20210107-WA1459.mp4/file' })
 break
 case 'dosa7':
-qute = fs.readFileSync('./gambar/fake.jpg') 
+qute = { url: 'https://telegra.ph/file/cb7371aa27942b81a3f61.jpg' }
 hisoka.sendMessage(from, qute, image, { quoted: mek, caption: '*SEMOGA DI BERI HIDAYAH*\nLink Download \n\nhttps://www.mediafire.com/file/3scnim6d1x4b8ie/VID-20210107-WA1461.mp4/file' })
 break
 case 'dosa8':	
-qute = fs.readFileSync('./gambar/fake.jpg') 
+qute = { url: 'https://telegra.ph/file/cb7371aa27942b81a3f61.jpg' }
 hisoka.sendMessage(from, qute, image, { quoted: mek, caption: '*SEMOGA DI BERI HIDAYAH*\nLink Download \n\nhttps://www.mediafire.com/file/dx9tklonu0eq36w/VID-20210107-WA1464.mp4/file' })
 break
 
